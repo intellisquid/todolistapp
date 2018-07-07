f91aac71da4d126ae3255683f164429693d43616
William Hendricks <wshendricks@gmail.comgit config --global user.email wshendricks@gmail.comgit config --global user.email wshendricks@gmail.com>
2018-07-07 11:49:02 -0500

    All functionality from tutorial is working now!

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -32,4 +32,10 @@ export class TodoComponent implements OnInit {
         itemTitle.value = null;
 
     }
+    alterCheck($key: string,isChecked) {
+        this.toDoService.checkOrUnCheckTitle($key,!isChecked);
+    }
+    onDelete($key: string){
+        this.toDoService.removeTitle($key);
+    }
 }
